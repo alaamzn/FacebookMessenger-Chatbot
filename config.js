f0b6b64c61371c0b9f628dfe9e69c4a283085b14
alaamezni <alaamezni96@gmail.com>
2019-10-31 08:50:03 +0100

    deteting the keys from config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-    FB_PAGE_TOKEN: 'EAAH7Gc3Ag5wBADWJgHa6u4rV89XyPsQRPG4Ux0HVSkGDcpjIrCqWMg2dsw8ZBcbTcMIXpyejWXZB0SPVNZCp43d7ZBF27kpmt4bP9ZBh04zQr572ll78G3IC25EYYZCbhPk02CUEOuYpTtpb8raWdrMScgD4jFQtZCHpBscqKxUWxdTfOrsF9ZCZB',
-    FB_VERIFY_TOKEN: 'alongstringforaiindeed01',
-    API_AI_CLIENT_ACCESS_TOKEN: '2c91baedb5764dd4acfd5b844eb0e430',
-    FB_APP_SECRET: '20e77dd3122acb4967c7786ae10e134a',
-    SERVER_URL: "https://aiindeed01.herokuapp.com/"
-};
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_SECRET: '',
+    SERVER_URL: ''
+}
